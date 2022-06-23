fe02a2a68c23dea90825a9b3361562e8271e765c
ViniciusMFernandes <viniawp@gmail.com>
2022-06-23 13:53:16 -0300

    desafios 003 e 004

---
+++ desafio003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite o 1º número: '))
+n2 = int(input('Digite o 2º número: '))
+s = n1 + n2
+print('A soma entre {}+{}={}'.format(n1, n2, s))
---
+++ desafio004.py
@@ -0,0 +1,5 @@
+a = input('Digite algo:')
+print('O que você digitou é: \n'
+      'Apenas letras? {}\n''Apenas números? {}\n''Apenas minusculas? {}\n'
+      'Apenas maiusculas? {}\n''Alfanumerico? {}\n''Espaços? {}'.format(a.isalpha(), a.isnumeric(), a.islower(),
+                                                                            a.isupper(), a.isalnum(), a.isspace()))
